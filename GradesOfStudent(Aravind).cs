97da9768b8a0d41f63776305ce2f06ded91007e9
Aravindx <109063587+Aravindx@users.noreply.github.com>
2022-07-12 18:54:34 +0530

    Add files via upload

---
+++ GradesOfStudent(Aravind).cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace grades_of_students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int loop = 0;
+            do
+            {
+                string input;
+                
+                Console.WriteLine("GRADE OF A STUDENT");
+                Console.WriteLine("__________________\n");
+                Console.WriteLine("Input the grade:");
+                input = Console.ReadLine();
+
+                if (input == "E" || input == "e")
+                {
+                    Console.WriteLine("Excellent");
+                }
+                else if (input == "V" || input == "v")
+                {
+                    Console.WriteLine("Very Good");
+                }
+                else if (input == "G" || input == "g")
+                {
+                    Console.WriteLine("Good");
+                }
+                else if (input == "A" || input == "a")
+                {
+                    Console.WriteLine("Average");
+                }
+                else if (input == "F" || input == "f")
+                {
+                    Console.WriteLine("Fail");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+                Console.WriteLine("Press 1 to continue");
+                loop = Convert.ToInt32(Console.ReadLine());
+
+            } while (loop==1);
+        }
+    }
+}
