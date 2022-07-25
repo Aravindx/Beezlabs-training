41277854a78ae4ec3a2895b41f7684ef2b7b0291
Aravindx <109063587+Aravindx@users.noreply.github.com>
2022-07-25 13:11:49 +0530

    Add files via upload

---
+++ ClassCar(Aravind).cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1evaluationOnClass
+{
+    interface car
+    {
+        float GetMilage();
+    }
+
+    class SUV : car
+    {
+        public float GetMilage()
+        {
+            float Milage = 14;
+            return Milage;        }
+    }
+
+    class SEDAN : car
+    {
+        public float GetMilage()
+        {
+            float Milage = 18;
+            return Milage;
+        }
+    }
+    class HATCHBACK : car
+    {
+        public float GetMilage()
+        {
+            float mileage = 25;
+            return mileage;
+        }
+    }
+    class mileage
+    {
+        public static void Main(String[] args)
+        {
+            HATCHBACK car1 = new HATCHBACK();
+            SEDAN car2 = new SEDAN();
+            Console.WriteLine(car1.GetMilage());
+            Console.WriteLine(car2.GetMilage());
+
+            Console.ReadKey();
+        }
+    }
+
+    
+}
+
