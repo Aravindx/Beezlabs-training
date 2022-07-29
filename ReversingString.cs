e69306687a2f721fe028f0e3a3cdc778191b369c
Aravindx <109063587+Aravindx@users.noreply.github.com>
2022-07-29 13:30:12 +0530

    Add files via upload

---
+++ ReversingString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ReversingString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Sentence that need to be Reversed");
+            string sentence = Console.ReadLine();
+            string[] word = sentence.Split(" ");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("The reversed sentence is:");
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+               
+                Console.Write("\t{0}",word[i]);
+
+            }
+        }
+    }
+}
